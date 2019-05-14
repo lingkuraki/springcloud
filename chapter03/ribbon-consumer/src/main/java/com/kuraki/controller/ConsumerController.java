8da086c72d5fa94d0a75c1c44725110eb8f07233
kuraki <583378006@qq.com>
2019-05-14 21:00:35 +0800

    ribbon

--- chapter03/ribbon-consumer/src/main/java/com/kuraki/controller/ConsumerController.java
+++ chapter03/ribbon-consumer/src/main/java/com/kuraki/controller/ConsumerController.java
@@ -1,19 +1,20 @@
 package com.kuraki.controller;
 
+import com.kuraki.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
 
     @Autowired
-    RestTemplate restTemplate;
+    private HelloService helloService;
+
 
     @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
     public String helloConsumer() {
-        return restTemplate.getForEntity("http://hello-service/hello", String.class).getBody();
+        return helloService.helloService();
     }
 }
