813341ca27fade31800d1407291a628b3a462ca1
kuraki <583378006@qq.com>
2019-05-16 22:30:26 +0800

    hystrix study in deep

--- chapter05/hystrix-consumer/src/main/java/com/kuraki/bean/UserCommand.java
+++ chapter05/hystrix-consumer/src/main/java/com/kuraki/bean/UserCommand.java
@@ -1,6 +1,9 @@
 package com.kuraki.bean;
 
 import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixThreadPoolKey;
 import org.springframework.web.client.RestTemplate;
 
 public class UserCommand extends HystrixCommand<User> {
@@ -9,8 +12,14 @@ public class UserCommand extends HystrixCommand<User> {
 
     private Long id;
 
+    public UserCommand() {
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("CommandGroupKey"))
+                .andCommandKey(HystrixCommandKey.Factory.asKey("CommandKey"))
+                .andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("ThreadPoolKey")));
+    }
 
-    protected UserCommand(Setter setter, RestTemplate restTemplate, Long id) {
+
+    public UserCommand(Setter setter, RestTemplate restTemplate, Long id) {
         super(setter);
         this.restTemplate = restTemplate;
         this.id = id;
@@ -19,5 +28,10 @@ public class UserCommand extends HystrixCommand<User> {
     @Override
     protected User run() throws Exception {
         return restTemplate.getForObject("http://user-service/users/{1}", User.class, id);
+    }
+
+    @Override // 重写该方法，开启缓存
+    protected String getCacheKey() {
+        return String.valueOf(id);
     }
 }
--- chapter05/hystrix-consumer/src/main/java/com/kuraki/service/UserService.java
+++ chapter05/hystrix-consumer/src/main/java/com/kuraki/service/UserService.java
@@ -1,14 +1,20 @@
 package com.kuraki.service;
 
 import com.kuraki.bean.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.netflix.hystrix.contrib.javanica.annotation.ObservableExecutionMode;
+import com.netflix.hystrix.contrib.javanica.cache.annotation.CacheKey;
+import com.netflix.hystrix.contrib.javanica.cache.annotation.CacheResult;
 import com.netflix.hystrix.contrib.javanica.command.AsyncResult;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import rx.Observable;
 
+import java.util.List;
 import java.util.concurrent.Future;
 
 @Service
@@ -18,7 +24,7 @@ public class UserService {
     private RestTemplate restTemplate;
 
     // 同步执行
-    @HystrixCommand
+    @HystrixCommand(commandKey = "getUserById", groupKey = "UserGroup", threadPoolKey = "getUserByIdThread")
     public User getUserById(Long id) {
         return restTemplate.getForObject("http://localhost:8090/users/{1}", User.class, id);
     }
@@ -39,7 +45,7 @@ public class UserService {
         return Observable.create(observer -> {
             try {
                 if (!observer.isUnsubscribed()) {
-                    User user = restTemplate.getForObject("http://user-service/user/{1}", User.class, id);
+                    User user = restTemplate.getForObject("http://user-service/users/{1}", User.class, id);
                     observer.onNext(user);
                     observer.onCompleted();
                 }
@@ -47,6 +53,34 @@ public class UserService {
                 observer.onError(e);
             }
         });
+    }
+
+    /**
+     * 请求缓存
+     */
+    @CacheResult(cacheKeyMethod = "getUserByIdCacheKey")
+    @HystrixCommand
+    public User findUserById(@CacheKey("id") Long id) {
+        return restTemplate.getForObject("http://user-service/users/{1}", User.class, id);
+    }
+
+    private Long getUserByIdCacheKey(Long id) {
+        return id;
+    }
+
+    /**
+     * 请求合并
+     */
+    @HystrixCollapser(batchMethod = "findAll", collapserProperties = {
+            @HystrixProperty(name = "timeDelayInMilliseconds", value = "100")
+    })
+    public User find(Long id) {
+        return null;
+    }
+
+    @HystrixCommand
+    public List<User> findAll(List<Long> ids) {
+        return restTemplate.getForObject("http://user-service/users?id={1}", List.class, StringUtils.join(ids, ","));
     }
 
 }
