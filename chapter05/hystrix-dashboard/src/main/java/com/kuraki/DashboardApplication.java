cae0c78bcb5a85d1ae254a61eeae0093d891216c
kuraki <583378006@qq.com>
2019-05-19 10:51:16 +0800

    update dashboard

--- chapter05/hystrix-dashboard/src/main/java/com/kuraki/DashboardApplication.java
+++ chapter05/hystrix-dashboard/src/main/java/com/kuraki/DashboardApplication.java
@@ -2,13 +2,13 @@ package com.kuraki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringCloudApplication
-public class HystrixApplication {
+public class DashboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HystrixApplication.class, args);
+        SpringApplication.run(DashboardApplication.class, args);
     }
 }
