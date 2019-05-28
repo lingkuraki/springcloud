3deaeeeb43608107b07a51c42d2b3cb6c3570fa3
kuraki <583378006@qq.com>
2019-05-28 22:37:03 +0800

    update for zuul

---
+++ chapter07/api-gateway-dynamic-route/src/main/java/com/kuraki/chapter07/ZuulApplication.java
@@ -0,0 +1,24 @@
+package com.kuraki.chapter07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @RefreshScope
+    @ConfigurationProperties("zuul")
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
+    }
+
+}
