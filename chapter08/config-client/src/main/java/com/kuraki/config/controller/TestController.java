46d14c1cdb4d15bd0113ecc62fc0e3489df5731c
kuraki <583378006@qq.com>
2019-06-03 08:57:47 +0800

    update config-client

--- chapter08/config-client/src/main/java/com/kuraki/config/controller/TestController.java
+++ chapter08/config-client/src/main/java/com/kuraki/config/controller/TestController.java
@@ -1,4 +1,29 @@
 package com.kuraki.config.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
 public class TestController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/hello")
+    public String from01() {
+        return env.getProperty("from", "undefined");
+    }
 }
